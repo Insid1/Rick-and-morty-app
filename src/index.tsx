81c41c0ebdb3951988cb583236a0d4609700badd
Oleg <oleg.volobuev@scrile.com>
2022-08-06 16:50:57 +0700

    installs MUI

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,10 @@ import './style.css';
 import ReactDOM from 'react-dom/client';
 import React from 'react';
 import App from './app/app';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
